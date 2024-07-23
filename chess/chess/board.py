900a4eb0e958558b8a4c20feaf51556ba106c211
jburn20 <48035436+jburn20@users.noreply.github.com>
2024-07-23 12:04:49 -0400

    Move validation can return piece on the board in response to player's inputted coordinates.

--- chess/chess/board.py
+++ chess/chess/board.py
@@ -3,12 +3,11 @@ class Board:
         self.board = self.create_board()
 
     def create_board(self):
+        board = [['# ' for _ in range(8)] for _ in range(8)]
+        # ^ create board, now we can modify with board[] statements 
         # Initialize the board with pieces
         specialrow = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
         pawns = ['p' for _ in range(8)]
-        print(pawns)
-        board = [['# ' for _ in range(8)] for _ in range(8)]
-        # ^ create board, now we can modify with board[] statements 
         board[0] = ['w' + x for x in specialrow] # we can set our bottom and top rows to have special rows
         board[1] = ['w' + j for j in pawns]
         board[7] = ['b' + i for i in specialrow] ## iteration
@@ -17,6 +16,38 @@ class Board:
     def print_board(self):
         for row in self.board:
             print(' '.join(row)) # Board is a list of 8 sublists with each list containing 1 row.
+        print()
+
+    def coord_converter(self,coord):
+        column, row = coord
+        col_index = ord(column) - ord("a")
+        row_index = 8 - int(row)
+        return (row_index,col_index)
+    def get_move(self):
+        move = input("Make your move: ")
+        x,y = move.split()
+        return self.coord_converter(x), self.coord_converter(y)
+    
+    def is_valid(self,start,end):
+        piece = self.board[start[0]][start[1]]
+        desire = self.board[end[0]][end[1]]
+        empty = '#'
+        print(desire)
+        if empty in desire:
+            print("clear")
+        if 'p' in piece:
+            print("pawn")
+        if 'R' in piece:
+            print("rook")
+    def make_move(self,start,end):
+        pass
+"""
+1. Move validation:
+-- Find piece with .is_valid()
+-- Implement logic such as pawn cannot move farther than 1 space
+2. Move logic
+3. Update board with move logic
+"""
     
     
     
--- chess/chess/game.py
+++ chess/chess/game.py
@@ -6,9 +6,8 @@ def main():
     while gameOn:
         myboard = Board()
         myboard.print_board()
-        
-        player1 = input("Enter the x and y: ")
-
-        
+        player1 = myboard.get_move() # a tuple formatted like ((x1,y1)(x2,y2)) then splits into start and end values
+        print(player1[0],player1[1]) #player1[0] is the first input coord, e.g 'e2', player[1] is the second input coord, e.g 'e4'
+        myboard.is_valid(player1[0],player1[1]) 
 if __name__ == "__main__":
     main()
